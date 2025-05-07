f45effe3ceda5bcbb9ed2e7e35f3a9f9bd85e299
vascocarlos <carlos.vasco@lemonsolutions.es>
2025-05-07 19:25:31 +0200

    dia 7 v

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 function mostrar(id) {
+  // Oculta todos los módulos
   document
     .querySelectorAll(".modulo")
     .forEach((m) => (m.style.display = "none"));
-  document.getElementById(id).style.display = "block";
+
+  // Muestra el módulo seleccionado
+  const modulo = document.getElementById(id);
+  modulo.style.display = "block";
+
+  // Desplaza suavemente hacia ese módulo
+  modulo.scrollIntoView({ behavior: "smooth" });
 
   if (id === "crv") rellenarResumenInmueble();
 }
@@ -34,16 +41,12 @@ function borrarDatos() {
 }
 
 function toggleTooltip(element) {
-  // Cierra todos los tooltips abiertos
   document.querySelectorAll(".tooltip-wrapper").forEach((el) => {
     if (el !== element) el.classList.remove("active");
   });
-
-  // Activa el tooltip clicado
   element.classList.toggle("active");
 }
 
-// Cierra tooltip si haces clic fuera
 document.addEventListener("click", function (e) {
   if (!e.target.closest(".tooltip-wrapper")) {
     document
